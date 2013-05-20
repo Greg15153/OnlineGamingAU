5b3a93d6cb5e339c409878932cdcba422ab452fb
Gregory Ellis <gellis@arcadia.edu>
2013-05-20 12:29:25 -0400

    Created the basis for everything
    Started working on the foundations of the website. CSS and navigation
    for each page.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+
+<head>
+
+<title>Arcadia University Online Gaming Club</title>
+
+<?php
+	require_once 'templates/header.php';
+?>
+
+</head>
+
+
+<body>
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8" />
+<meta name="description" content="AU Online Gaming Club" />
+<meta name="keywords" content="cereal, gamers, arcadia, university, online, gaming, gamers, club" />
+
+<link rel="icon" type="image/ico" href="images/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="css/mainCSS.css" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+
+<div id="navBar">
+	<div id="navLinks">
+		<img src="images/logoPlaceHolder.png" /><a href='index.php'>Home</a> |
+		<a href='members.php'>Members</a> |
+		<a href='games.php'>Games</a> |
+		<a href='signIn.php'>Sign in</a> |
+		<a href='register.php'>Register</a>
+	</div>
+</div>
