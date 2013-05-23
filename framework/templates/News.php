849f5867824093c04e6bf093d033505935840f88
Will LaRiccia <zrevgames@gmail.com>
2013-05-22 22:34:03 -0400

    cleaned up the code

--- framework/templates/News.php
+++ framework/templates/News.php
@@ -1,48 +1,49 @@
 <link rel="stylesheet" type="text/css" href="css/newsPosts.css" />
-	
-		
 <?php
 	global $mysql;
-	if(!isset($_GET['id']) | @$_GET['id'] == ""){
+	if(!isset($_GET['id']) || $_GET['id'] == ""){
+		$result = $mysql->getArray("SELECT * FROM  newspost ORDER BY newsDate DESC");
 		
-		$sqlGetTitles = "SELECT * FROM  `newspost` ORDER BY newsDate DESC;";
-		$result = $mysql -> getArray($sqlGetTitles);
-		$totalPosts = count($result);
-		echo "<div class='pageTitle'><h2>News Archive</h2></div>";
-		echo "<table id='newsTable'>
+?>
+		<div class='pageTitle'><h2>News Archive</h2></div>
+		<table id='newsTable'>
 			  <tr>
 				<th>News Title</th>
 				<th>News Poster</th>
 				<th>Posted Date</th>
-			  </tr>";
-	
-		for($i=0; $i<$totalPosts; $i++){
-			$postDate = date('M d, Y' , strtotime($result[$i]['newsDate']));
-			$sqlGetPoster = "SELECT username FROM `userInformation` WHERE userID = '".$result[$i]['newsPoster']."'";
-			$result2 = $mysql ->getArray($sqlGetPoster);
-	
-			echo "<tr>
-					<td><a href='?do=News&id=".$result[$i]['newsID']."'>".$result[$i]['newsTitle']."</a></td>
-					<td><a href='?do=Profile&id='".$result[$i]['newsPoster']."'>".$result2[0]['username']."</a></td>
-					<td>".$postDate."</td>
-				  </tr>";
+			  </tr>
+	<?
+		foreach($result as $post){
+			
+			$postDate = date('M d, Y' , strtotime($post['newsDate']));
+			$poster = $post['newsPoster'];
+			$result2 = $mysql ->getArray("SELECT username FROM userInformation WHERE userID='$poster'");
+	?>
+		<tr>
+					<td><a href='?do=News&id=<?=$post['newsID']?>'><?=$post['newsTitle']?></a></td>
+					<td><a href='?do=Profile&id=<?=$post['newsPoster']?>'><?=$result2[0]['username']?></a></td>
+					<td><?=$postDate?></td>
+				  </tr>
+	<?
 		}
-			echo "</table>";
+	?>
+		</table>
+	<?
 	}
 	else{
-		$sqlGetNews = "SELECT * FROM  `newspost` WHERE `newsID` = ".$_GET['id'].";";
-		$result = $mysql -> getArray($sqlGetNews);
-		
-		$sqlGetPoster = "SELECT username FROM `userInformation` WHERE userID = '".$result[0]['newsPoster']."'";
-		$result2 = $mysql ->getArray($sqlGetPoster);
+		$result = $mysql -> getArray("SELECT * FROM  `newspost` WHERE `newsID` = ".$_GET['id'].";");
+		$result2 = $mysql ->getArray("SELECT username FROM `userInformation` WHERE userID = '".$result[0]['newsPoster']."'");
 		$postDate = date('M d, Y' , strtotime($result[0]['newsDate']));
-		echo "<div class='newsContainer'>";
-		echo "<div class='newsTitle'><a href='?do=News&id=".$result[0]['newsID']."'>".$result[0]['newsTitle']."</a>";
-		echo "<div class='newsPoster'><a href='?do=Profile&id=".$result[0]['newsPoster']."'>".$result2[0]['username']." on ".$postDate."</a></div></div>";
-		echo "<div class='newsContent'><p>&ensp;".$result[0]['newsContent']."</p></div>";
-		echo "</div>";
+	?>
 	
-		echo "<div class='pageTitle'><a href='?do=News'>Go back to archive</a></div>";
+		<div class='newsContainer'>
+		<div class='newsTitle'><a href='?do=News&id=<?=$result[0]['newsID']?>'><?=$result[0]['newsTitle']?></a>
+		<div class='newsPoster'><a href='?do=Profile&id=<?$result[0]['newsPoster=']?>'><?=$result2[0]['username']?> on <?=$postDate?></a></div></div>
+		<div class='newsContent'><p>&ensp;<?=$result[0]['newsContent']?></p></div>
+		</div>
 	
+		<div class='pageTitle'><a href='?do=News'>Go back to archive</a></div>
+		
+	<?
 	}
 ?>
