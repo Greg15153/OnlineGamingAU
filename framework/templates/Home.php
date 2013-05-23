7a02f6c88d99cdad718b5ba47cd977e54f28dc40
Will LaRiccia <zrevgames@gmail.com>
2013-05-22 22:59:39 -0400

    cleaned up index code

--- framework/templates/Home.php
+++ framework/templates/Home.php
@@ -1,4 +1,6 @@
-	
+<?
+	global $mysql;
+?>
 	<div id="sliderContainer">
 		<div id="slider">
 			<div id="slides">
@@ -6,7 +8,6 @@
 					<a href="#" id="prev">prev</a>
 					<a href="#" id="next">next</a>
 				</div>
-				
 				<ul>
 					<li><img class="sliderImage" src="images/slider/Slider/officers.png" alt="2013-2014 Officers"/>
 						<div class="textOverlay">Meet the Officers for the 2013-2014 school year!</div></li>
@@ -17,30 +18,25 @@
 					<li><img class="sliderImage" src="images/slider/Slider/teemoHats.png" />
 						<div class="textOverlay">Upcoming event: March 23rd, 2013 League of Legends tournement</div></li>
 				</ul>
-				
 			</div>
 		</div>
 	</div>
-	
-	<link rel="stylesheet" type="text/css" href="css/newsPosts.css" />
-
-<?php
-	global $mysql;
-	$sqlGetNews = "SELECT * FROM  `newspost` ORDER BY newsDate DESC LIMIT 5;";
-	$result = $mysql -> getArray($sqlGetNews);
-	for($i = 0; $i<=5; $i++){
-		if(@$result[$i]['newsID'] != null){
-		$sqlGetPoster = "SELECT username FROM `userInformation` WHERE userID = '".$result[$i]['newsPoster']."'";
-		$result2 = $mysql ->getArray($sqlGetPoster);
-		$postDate = date('M d, Y' , strtotime($result[$i]['newsDate']));
-		echo "<div class='newsContainer'>";
-		echo "<div class='newsTitle'><a href='?do=News&id=".$result[$i]['newsID']."'>".$result[$i]['newsTitle']."</a>";
-		echo "<div class='newsPoster'><a href='?do=Profile&id=".$result[$i]['newsPoster']."'>".$result2[0]['username']." on ".$postDate."</a></div></div>";
-		echo "<div class='newsContent'><p>&ensp;".$result[$i]['newsContent']."</p></div>";
-		echo "</div>";
+	<div class="news">
+<?
+	$result = $mysql -> getArray("SELECT * FROM  newspost ORDER BY newsDate DESC LIMIT 5");
+	foreach($result as $post){
+		if($post['newsID'] != null){
+		$postedBy = $post['newsPoster'];
+		$result2 = $mysql->getArray("SELECT username FROM userInformation WHERE userID='$postedBy'");
+		$postDate = date('M d, Y' , strtotime($post['newsDate']));
+	?>
+		<div class='newsContainer'>
+			<div class='newsTitle'><a href='?do=News&id=<?=$post['newsID']?>'><?=$post['newsTitle']?></a>
+			<div class='newsPoster'><a href='?do=Profile&id=<?=$post['newsPoster']?>'><?=$result2[0]['username']?> on <?=$postDate?></a></div></div>
+			<div class='newsContent'><p>&ensp;<?=$post['newsContent']?></p></div>
+		</div>
+	<?
 		}
 	}
 ?>
-
-		
-		
+	</div>
